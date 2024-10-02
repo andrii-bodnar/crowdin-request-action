1d3ea84dbf4ea27a04523a0a5b9bed3db2066151
Vladimir Starkov <iamstarkov@gmail.com>
2024-10-02 15:27:17 +0200

    fix: add method in log group to distringuis POST from GET

--- src/requester.ts
+++ src/requester.ts
@@ -98,7 +98,7 @@ export class Requester {
         const requestConfig = this.getRequestConfig(contentType);
 
         core.startGroup('Request');
-        core.info(requestUrl);
+        core.info(`${method.toUpperCase()} ${requestUrl}`);
         this.debugObject(requestConfig);
         this.query && this.debugObject(this.query);
         this.body && !isStorageAdding && this.debugObject(this.body);
